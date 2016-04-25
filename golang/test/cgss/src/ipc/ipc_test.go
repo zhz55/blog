d2c0cb1745965eab4a905a4c90a588ca309aa692
zhz55 <kasim@126.com>
2016-04-25 14:46:08 +0800

    cg part

--- golang/test/cgss/src/ipc/ipc_test.go
+++ golang/test/cgss/src/ipc/ipc_test.go
@@ -16,5 +16,18 @@ func (server *EchoServer) Name() string {
 }
 
 func TestIpc(t *testing.T) {
-    
+    server := NewIpcServer(&EchoServer{})
+
+    client1 := NewIpcClient(server)
+    client2 := NewIpcClient(server)
+
+    resp1 := client1.Call("From Client1")
+    resp2 := client2.Call("From Client2")
+
+    if resp1 != "ECHO:From Client1" || resp2 != "ECHO:FROM Client2" {
+        t.Error("IpcClient.Call failed. resp1:", resp1, "resp2:", resp2)
+    }
+
+    client1.Close()
+    client2.Close()
 }
